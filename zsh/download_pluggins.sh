cc4f559f70743dc99b5d880092b6ab2bdc50207b
Tatsuhiro Onodera <onoderat@stanford.edu>
2018-07-31 02:38:23 -0700

    oh-my-zsh is added as a download

--- zsh/download_pluggins.sh
+++ zsh/download_pluggins.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 git clone --depth=1 https://github.com/tarjoilija/zgen.git $HOME/.zgen
 git clone --depth=1 https://github.com/junegunn/fzf.git $HOME/.fzf
 ~/.fzf/install
