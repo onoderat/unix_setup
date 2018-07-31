220bece52d9a4c6eabe4d0e9904c04bb3b4f5a25
Tatsuhiro Onodera <onoderat@sh-ln08.stanford.edu>
2018-07-31 01:59:57 -0700

    fixed .zshrc with minimal theme
    strange black box bug shows up in terminal. To fix later.

--- download_zsh.sh
+++
@@ -1,3 +0,0 @@
-git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
-git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
-
--- zsh/download_pluggins.sh
+++ zsh/download_pluggins.sh
@@ -1,5 +1,4 @@
-#!usr/bin/bash
-git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+#!/usr/bin/bash
 git clone --depth=1 https://github.com/tarjoilija/zgen.git $HOME/.zgen
 git clone --depth=1 https://github.com/junegunn/fzf.git $HOME/.fzf
 ~/.fzf/install
